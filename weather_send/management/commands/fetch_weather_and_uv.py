edad5ea31f74015e50e7aa4982b61e5da28cc0b5
Drew Esse <andrewesse404@gmail.com>
2023-09-06 12:43:31 -0400

    updates

--- weather_send/management/commands/fetch_weather_and_uv.py
+++ weather_send/management/commands/fetch_weather_and_uv.py
@@ -10,51 +10,50 @@ class Command(BaseCommand):
         weather_api_key = os.environ.get('WEATHER_API_KEY')
         lat = os.environ.get('LATITUDE')
         lon = os.environ.get('LONGITUDE')
-        weather_url = f'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={weather_api_key}&units=imperial'
-        uv_url = f'http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid={weather_api_key}'
+        one_call_url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude=current,minutely,hourly&appid={weather_api_key}&units=imperial"
 
         try:
-            weather_data = requests.get(weather_url).json()
-            uv_data = requests.get(uv_url).json()
-            print(weather_data)
-            print("UV API Raw Response:", uv_data)
+            one_call_data = requests.get(one_call_url).json()
+            today_forecast = one_call_data.get('daily', [{}])[0]
 
-            if weather_data.get('cod') != 200:
-                self.stdout.write(
-                    self.style.ERROR(f"Weather API Error: {weather_data.get('message', 'Unknown error')}"))
-                return None, None, None, f"Weather API Error: {weather_data.get('message', 'Unknown error')}"
-            if 'value' not in uv_data:
-                self.stdout.write(self.style.ERROR(f"UV API Error: {uv_data.get('message', 'Unknown error')}"))
-                return None, None, None, f"UV API Error: {uv_data.get('message', 'Unknown error')}"
+            if not today_forecast:
+                self.stdout.write(self.style.ERROR('Failed to fetch weather data.'))
+                return None, None, None, None, None
 
-            temperature = weather_data['main']['temp']
-            high = weather_data['main']['temp_max']
-            low = weather_data['main']['temp_min']
-            uv_index = uv_data['value']
+            day_temperature = round(today_forecast.get('temp', {}).get('day', "N/A"))
+            min_temperature = round(today_forecast.get('temp', {}).get('min', "N/A"))
+            max_temperature = round(today_forecast.get('temp', {}).get('max', "N/A"))
+            uv_index = round(today_forecast.get('uvi', "N/A"))
+            summary = today_forecast.get('summary', "N/A")
 
-            return temperature, high, low, uv_index
+            return day_temperature, min_temperature, max_temperature, uv_index, summary
 
         except requests.RequestException as e:
             self.stdout.write(self.style.ERROR(f"An error occurred while fetching data: {e}"))
-            return None, None, None, f"An error occurred while fetching data: {e}"
+            return None, None, None, None, None
 
-    def send_sms(self, temperature, high, low, uv_index):
+    def send_sms(self, day_temperature, min_temperature, max_temperature, uv_index, summary):
         account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
         auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
         client = Client(account_sid, auth_token)
 
-        body = f"Good morning! Today's temperature is {temperature}°F, with a high of {high}°F and a low of {low}°F. The UV index is {uv_index}."
+        recipient_numbers = [os.environ.get('RECIPIENT_PHONE1'), os.environ.get('RECIPIENT_PHONE2')]
 
-        message = client.messages.create(
-            body=body,
-            from_=os.environ.get('TWILIO_PHONE'),
-            to=os.environ.get('RECIPIENT_PHONE')
-        )
+        body = (f"Good morning! Today's temperature is {day_temperature}°F, "
+                f"with a high of {max_temperature}°F and a low of {min_temperature}°F. "
+                f"The UV index is {uv_index}. Summary: {summary}.")
+
+        for number in recipient_numbers:
+            message = client.messages.create(
+                body=body,
+                from_=os.environ.get('TWILIO_PHONE'),
+                to=number
+            )
 
     def handle(self, *args, **kwargs):
-        temperature, high, low, uv_index = self.fetch_weather_and_uv()
-        if temperature is not None and high is not None and low is not None and uv_index is not None:
-            self.send_sms(temperature, high, low, uv_index)
+        day_temperature, min_temperature, max_temperature, uv_index, summary = self.fetch_weather_and_uv()
+        if all([day_temperature, min_temperature, max_temperature, uv_index, summary]):
+            self.send_sms(day_temperature, min_temperature, max_temperature, uv_index, summary)
             self.stdout.write(self.style.SUCCESS('Successfully sent weather and UV index information.'))
         else:
             self.stdout.write(self.style.ERROR('Failed to fetch weather and UV index data.'))
