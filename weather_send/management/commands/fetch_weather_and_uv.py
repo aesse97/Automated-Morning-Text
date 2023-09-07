5c592f17a77e8cc9beddc242fe91f9077d845d6b
Drew Esse <andrewesse404@gmail.com>
2023-09-07 01:06:56 -0400

    moreppl

--- weather_send/management/commands/fetch_weather_and_uv.py
+++ weather_send/management/commands/fetch_weather_and_uv.py
@@ -3,28 +3,28 @@ from twilio.rest import Client
 import os
 import requests
 
+
 class Command(BaseCommand):
     help = 'Fetches weather and UV index and sends an SMS'
 
-    def fetch_weather_and_uv(self):
+    def fetch_weather_and_uv(self, lat, lon):
         weather_api_key = os.environ.get('WEATHER_API_KEY')
-        lat = os.environ.get('LATITUDE')
-        lon = os.environ.get('LONGITUDE')
         one_call_url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude=current,minutely,hourly&appid={weather_api_key}&units=imperial"
 
         try:
             one_call_data = requests.get(one_call_url).json()
             today_forecast = one_call_data.get('daily', [{}])[0]
+            print(today_forecast)
 
             if not today_forecast:
                 self.stdout.write(self.style.ERROR('Failed to fetch weather data.'))
                 return None, None, None, None, None
 
-            day_temperature = round(today_forecast.get('temp', {}).get('day', "N/A"))
-            min_temperature = round(today_forecast.get('temp', {}).get('min', "N/A"))
-            max_temperature = round(today_forecast.get('temp', {}).get('max', "N/A"))
-            uv_index = round(today_forecast.get('uvi', "N/A"))
-            summary = today_forecast.get('summary', "N/A")
+            day_temperature = round(today_forecast['temp']['day'])
+            min_temperature = round(today_forecast['temp']['min'])
+            max_temperature = round(today_forecast['temp']['max'])
+            uv_index = round(today_forecast['uvi'])
+            summary = today_forecast['summary']
 
             return day_temperature, min_temperature, max_temperature, uv_index, summary
 
@@ -32,18 +32,16 @@ class Command(BaseCommand):
             self.stdout.write(self.style.ERROR(f"An error occurred while fetching data: {e}"))
             return None, None, None, None, None
 
-    def send_sms(self, day_temperature, min_temperature, max_temperature, uv_index, summary):
+    def send_sms(self, phone_numbers, day_temperature, min_temperature, max_temperature, uv_index, summary):
         account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
         auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
         client = Client(account_sid, auth_token)
-
-        recipient_numbers = [os.environ.get('RECIPIENT_PHONE1'), os.environ.get('RECIPIENT_PHONE2')]
 
         body = (f"Good morning! Today's temperature is {day_temperature}°F, "
                 f"with a high of {max_temperature}°F and a low of {min_temperature}°F. "
                 f"The UV index is {uv_index}. Summary: {summary}.")
 
-        for number in recipient_numbers:
+        for number in phone_numbers:
             message = client.messages.create(
                 body=body,
                 from_=os.environ.get('TWILIO_PHONE'),
@@ -51,9 +49,15 @@ class Command(BaseCommand):
             )
 
     def handle(self, *args, **kwargs):
-        day_temperature, min_temperature, max_temperature, uv_index, summary = self.fetch_weather_and_uv()
-        if all([day_temperature, min_temperature, max_temperature, uv_index, summary]):
-            self.send_sms(day_temperature, min_temperature, max_temperature, uv_index, summary)
-            self.stdout.write(self.style.SUCCESS('Successfully sent weather and UV index information.'))
-        else:
-            self.stdout.write(self.style.ERROR('Failed to fetch weather and UV index data.'))
+        locations_and_numbers = [
+            {'lat': os.environ.get('LAT1'), 'lon': os.environ.get('LON1'),
+             'numbers': [os.environ.get('RECIPIENT_PHONE1'), os.environ.get('RECIPIENT_PHONE2')]},
+            {'lat': os.environ.get('LAT2'), 'lon': os.environ.get('LON2'),
+             'numbers': [os.environ.get('RECIPIENT_PHONE3')]}
+        ]
+        for loc in locations_and_numbers:
+            day_temperature, min_temperature, max_temperature, uv_index, summary = self.fetch_weather_and_uv(loc['lat'], loc['lon'])
+
+            if all([day_temperature, min_temperature, max_temperature, uv_index, summary]):
+                self.send_sms(loc['numbers'], day_temperature, min_temperature, max_temperature, uv_index, summary)
+                self.stdout.write(self.style.SUCCESS('Successfully sent weather and UV index information.'))
