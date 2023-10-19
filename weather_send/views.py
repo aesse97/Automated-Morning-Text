4d2ee1a04a64308adc8df84b8b598f2a36988eff
Drew Esse <andrewesse404@gmail.com>
2023-10-18 20:31:02 -0400

    improveai

--- weather_send/views.py
+++ weather_send/views.py
@@ -8,6 +8,7 @@ import requests
 openai.api_key = os.environ.get('OPENAI_API_KEY')
 ENGINE = "gpt-3.5-turbo"
 CUSTOM_REPLY_TRIGGER = os.environ.get('CUSTOM_REPLY_TRIGGER')
+conversations = []
 
 @csrf_exempt
 def sms_reply(request):
@@ -28,19 +29,37 @@ def sms_reply(request):
     return HttpResponse(str(response))
 
 def generate_ai_response(prompt):
-    messages = [
-        {"role": "system", "content": "You are a funny helpful assistant who enjoys making comedy."},
-        {"role": "user", "content": prompt}
-    ]
+    if prompt.strip().lower().startswith("image "):
+        image_prompt = prompt[6:].strip()  # Remove "image " from the prompt
+        return generate_dalle_image(image_prompt)
+
+    user_message = {"role": "user", "content": prompt}
+    conversations.append(user_message)
+
+    system_message = {"role": "system",
+                      "content": "You are a funny helpful assistant who enjoys comedy."}
+    messages = [system_message] + conversations
+    messages = messages[-10:]
 
     response = openai.ChatCompletion.create(
         model=ENGINE,
-        messages=messages
+        messages=messages,
+        temperature=1.0
     )
 
     assistant_message = response.choices[0].message.content
+    conversations.append({"role": "assistant", "content": assistant_message.strip()})
 
     return assistant_message.strip()
+
+def generate_dalle_image(prompt, size="1024x1024", n=1):
+    response = openai.Image.create(
+      prompt=prompt,
+      n=n,
+      size=size
+    )
+    image_url = response['data'][0]['url']
+    return image_url
 
 def send_pushover_notification(message):
     pushover_url = "https://api.pushover.net/1/messages.json"
